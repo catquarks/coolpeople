a937378de76f3d4c6850935a25730f3390b3d1fc
Tami <catquarks@gmail.com>
2017-03-22 01:47:01 -0400

    create seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322052855) do
+ActiveRecord::Schema.define(version: 20170322053855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170322052855) do
     t.string   "favorite_city"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "instrument"
   end
 
 end
